a58c71adc4f4bf5e00fbe39283d3b0a3579c93c4
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-03 19:27:23 -0600

    fix: change picture mutation parameter name

--- src/user/mutation/picture.mutation.user.ts
+++ src/user/mutation/picture.mutation.user.ts
@@ -12,14 +12,14 @@ import { IPermission } from '../../role/schema';
 import { UserModel } from '../model';
 import { IPictureInput, IUser, IUserSuccess } from '../schema';
 
-export const picture = async (_: object, args: { file: FileUpload, user: IPictureInput }, ctx: IContext): Promise<IUserSuccess | IError> => {
+export const picture = async (_: object, args: { file: FileUpload, picture: IPictureInput }, ctx: IContext): Promise<IUserSuccess | IError> => {
   try {
-    const { file, user } = args;
+    const { file, picture } = args;
     const { session } = ctx;
     let authorized: boolean;
 
-    if (user._id) {
-      authorized = await isAuthorized(session, IPermission.UPDATE_USER, user._id);
+    if (picture._id) {
+      authorized = await isAuthorized(session, IPermission.UPDATE_USER, picture._id);
     } else {
       authorized = await isAuthorized(session, IPermission.CREATE_USER);
     }
@@ -28,20 +28,20 @@ export const picture = async (_: object, args: { file: FileUpload, user: IPictur
       return unauthorized('You are not allowed to perform this action');
     }
 
-    let picture: string;
+    let pictureUrl: string;
     let uploadResult: UploadApiErrorResponse | UploadApiResponse;
 
-    if (user.url) {
-      const unique: string = getImageUnique(user.url, usersUploadOptions.folder);
+    if (picture.url) {
+      const unique: string = getImageUnique(picture.url, usersUploadOptions.folder);
       await AdminAPI.delete_resources([unique]);
     }
 
     if (file) {
       uploadResult = await uploadImage(file, usersUploadOptions);
-      picture = uploadResult.secure_url;
+      pictureUrl = uploadResult.secure_url;
     }
 
-    const userResult: IUser = await UserModel.findByIdAndUpdate(user._id, { picture }, { new: true });
+    const userResult: IUser = await UserModel.findByIdAndUpdate(picture._id, { picture: pictureUrl }, { new: true });
 
     return {
       user: userResult
