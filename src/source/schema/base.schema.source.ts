d9da13cdfb93fa585ddf5debb4ef7632f0496130
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-03 18:30:50 -0600

    feat: add missing _id on source schema

--- src/source/schema/base.schema.source.ts
+++ src/source/schema/base.schema.source.ts
@@ -1,12 +1,14 @@
 import { gql } from 'apollo-server-express';
 
 export interface ISource {
+  _id?: string;
   name: string;
   url: string;
 }
 
 export const BaseSchemaSource = gql`
   type Source {
+    _id: String
     name: String!
     url: String!
   }
