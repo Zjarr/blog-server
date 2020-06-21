56c6de0c5ab6abfe1c6d0b9e2670380002739f17
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-21 00:11:57 -0600

    fix: update context strict types

--- src/context/index.ts
+++ src/context/index.ts
@@ -4,17 +4,14 @@ import { verifyJWT } from '../lib/jwt';
 import { IUser } from '../user/schema';
 
 export interface IContext {
-  origin: string | string[];
-  session: IUser;
+  session: IUser | null;
 }
 
 export const context = (ctx: { req: Request }): IContext => {
   const { req: { headers } } = ctx;
-  const origin = headers.origin;
   const session = verifyJWT(headers.authorization);
 
   return {
-    origin,
     session
   };
 };
