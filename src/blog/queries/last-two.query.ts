6c0896bfe7039c24e8087deb4d143e0ed216069e
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-09-28 21:06:36 -0600

    feat: refactor blog last two query map categories function

--- src/blog/queries/last-two.query.ts
+++ src/blog/queries/last-two.query.ts
@@ -2,7 +2,7 @@ import { CategoryModel } from '../../category/model';
 import { ICategory } from '../../category/schema';
 import { IContext } from '../../context';
 import { IError } from '../../error/schema';
-import { isAuthorized } from '../../utils/functions';
+import { isAuthorized, mapCategories } from '../../utils/functions';
 import { unauthorized } from '../../utils/values';
 
 import { BlogModel } from '../model';
@@ -23,22 +23,6 @@ const createSearchQuery = (query: IGetBlogsLastTwoInput): ISearchQuery => {
   return searchQuery;
 };
 
-const mapCategoriesObject = (ids: string[], categories: ICategory[]): string => {
-  return categories
-    .filter(category => ids.includes(category._id!))
-    .map(category => category.name).join(' | ');
-};
-
-const mapBlogCategories = (blogs: IBlog[], categories: ICategory[]): IBlog[] => {
-  return blogs.map(blog => {
-    const categoriesString = mapCategoriesObject(blog.categories || [], categories);
-
-    blog.categoriesString = categoriesString;
-
-    return blog;
-  });
-};
-
 export const blogsLastTwo = async (_parent: object, args: { blogs: IGetBlogsLastTwoInput }, ctx: IContext): Promise<IBlogsSuccess | IError> => {
   const { blogs } = args;
   const { session } = ctx;
@@ -49,11 +33,9 @@ export const blogsLastTwo = async (_parent: object, args: { blogs: IGetBlogsLast
   }
 
   const searchQuery: ISearchQuery = createSearchQuery(blogs);
-
   const blogsFound: IBlog[] = await BlogModel.find(searchQuery).sort({ updated: -1 }).limit(2);
   const categoriesFound: ICategory[] = await CategoryModel.find({ active: true });
-
-  const blogsResponse: IBlog[] = mapBlogCategories(blogsFound, categoriesFound);
+  const blogsResponse: IBlog[] = mapCategories(blogsFound, categoriesFound);
 
   return {
     blogs: blogsResponse
